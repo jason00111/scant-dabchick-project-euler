cd235ce75e6c64d5500508c34dca64e159fb5663
Sylvan Swierkosz <sswierkosz@gmail.com>
2016-10-24 09:13:26 -0700

    add folder for success images with screenshot for problem 1 completion | add example solve for problem 1

--- src/problems/problem1.js
+++ src/problems/problem1.js
@@ -10,3 +10,17 @@ const multiplesOfThreeAndFive = (n) => {
 }
 
 module.exports = multiplesOfThreeAndFive
+
+
+//Example solved using Ruby:
+// class Integer
+//   def sum_mod modulus
+//     n = self.div modulus
+//     modulus * n * (n + 1) / 2
+//   end
+// end
+//
+// num = 999
+// sum = num.sum_mod(3) + num.sum_mod(5) - num.sum_mod(15)
+//
+// puts "Sum is #{ sum }."
